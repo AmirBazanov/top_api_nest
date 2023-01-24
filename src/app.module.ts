af5a66cce02e3ffa4c60686fbebd82eeb1ec4a28
Amir Bazanov <flash5651311@gmail.com>
2023-01-24 22:09:36 +0500

    Add mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
+import { TopPageModule } from './top-page/top-page.module';
+import { ReviewModule } from './review/review.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot(),
+		AuthModule, ProductModule,
+		TopPageModule, ReviewModule,
+		MongooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getMongoConfig
+		})],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
